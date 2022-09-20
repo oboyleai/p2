17082e7f31c1971955b83cd6ba3b238a69f6952a
oboyleai <oboyleai@umich.edu>
2022-09-19 22:43:27 -0400

    Starter files with function stubs

---
+++ resize.cpp
@@ -0,0 +1,11 @@
+// Project UID af1f95f547e44c8ea88730dfb185559d
+
+#include "Image.h"
+#include "processing.h"
+#include <iostream>
+#include "Matrix.h"
+#include <cmath>
+using namespace std;
+int main() {
+  cout << "Hello computer machine :D" << endl;
+}
