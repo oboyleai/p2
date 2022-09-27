ced1195d55348f868d60c5c534ce1e1769bd24eb
mileslow <mileslow@umich.edu>
2022-09-27 01:39:35 -0400

    going back from autograder

--- Image_tests.cpp
+++ Image_tests.cpp
@@ -4,12 +4,12 @@
 #include "Image_test_helpers.h"
 #include "unit_test_framework.h"
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
 #include <cassert>
 
 using namespace std;
-
 
 // Here's a free test for you! Model yours after this one.
 // Test functions have no interface and thus no RMEs, but
@@ -17,7 +17,8 @@ using namespace std;
 // -----
 // Sets various pixels in a 2x2 Image and checks
 // that Image_print produces the correct output.
-TEST(test_print_basic) {
+TEST(test_print_basic)
+{
   Image *img = new Image; // create an Image in dynamic memory
 
   const Pixel red = {255, 0, 0};
@@ -40,10 +41,17 @@ TEST(test_print_basic) {
   correct << "P3\n2 2\n255\n";
   correct << "255 0 0 0 255 0 \n";
   correct << "0 0 255 255 255 255 \n";
+  // cout << Image_get_pixel(img, 1, 1).;
+  // cout << endl
+  //      << endl
+  //      << endl;
+  // Image_print(img, cout);
   ASSERT_EQUAL(s.str(), correct.str());
 
   delete img; // delete the Image
 }
+
+// make max size tests
 
 // IMPLEMENT YOUR TEST FUNCTIONS HERE
 // You are encouraged to use any functions from Image_test_helpers.h as needed.
@@ -54,4 +62,46 @@ TEST(test_print_basic) {
 // standard, the version of g++ we are using reports a warning
 // when there is a semicolon. Therefore do NOT add a semicolon
 // after TEST_MAIN()
+
+// no idea how to test this
+// TEST(testInitWFile)
+// {
+
+//   Image *img = new Image; // create an Image in dynamic memory
+
+//   // const Pixel red = {255, 0, 0};
+//   // const Pixel green = {0, 255, 0};
+//   // const Pixel blue = {0, 0, 255};
+//   // const Pixel white = {255, 255, 255};
+
+//   string fileName = "dog.ppm";
+//   ifstream fin;
+//   // fin.open(fileName);
+//   fin.open(fileName);
+//   Image_init(img, fin);
+//   // ASSERT_EQUAL(Image_equal(img))
+// }
+
+// ba
+
+TEST(testSetGetPixelBasic)
+{
+  Image *img = new Image; // create an Image in dynamic memory
+
+  const Pixel white = {255, 255, 255};
+
+  Image_init(img, 2, 2);
+  Image_set_pixel(img, 1, 1, white);
+
+  // assert(Image_get_pixel(img, 1, 1))
+
+  Pixel imgPxl = Image_get_pixel(img, 1, 1);
+
+  ASSERT_EQUAL(imgPxl.r, 255);
+  ASSERT_EQUAL(imgPxl.g, 255);
+  ASSERT_EQUAL(imgPxl.b, 255);
+
+  delete img; // delete the Image
+}
+
 TEST_MAIN() // Do NOT put a semicolon here
